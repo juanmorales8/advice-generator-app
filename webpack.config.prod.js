18b4cfaeeec2cbe332fe0063f57f47493c9aba80
juanmora8 <juan.morales.lpz187@gmail.com>
2022-03-28 14:58:41 -0600

    Adicion de archivo de configuracion de webpack en modo produccion

---
+++ webpack.config.prod.js
@@ -0,0 +1,84 @@
+const path = require("path");
+const miniCssExtrarPlugin = require("mini-css-extract-plugin");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const cssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+const terserWebpackPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@images": path.resolve(__dirname, "src/assets/images"),
+      "@fonts": path.resolve(__dirname, "src/assets/fonts"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@containers": path.resolve(__dirname, "src/containers"),
+      "@customHooks": path.resolve(__dirname, "src/customHooks"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@api": path.resolve(__dirname, "src/api"),
+    },
+  },
+
+  mode: "production",
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            /*Allow using react without import it in each file*/
+            presets: [["@babel/preset-react", { runtime: "automatic" }]],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: [miniCssExtrarPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg)/i,
+        type: "asset/resource",
+        generator: { filename: "./assets/images/[hash][ext][query]" },
+      },
+      {
+        test: /\.(woff2)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/fonts/[hash][ext][query]",
+        },
+      },
+    ],
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [new cssMinimizerWebpackPlugin(), new terserWebpackPlugin()],
+  },
+
+  plugins: [
+    new htmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "index.html",
+      favicon: "./public/favicon.png",
+    }),
+    new miniCssExtrarPlugin({
+      filename: "assets/styles/[name].[contenthash].css",
+    }),
+  ],
+};
